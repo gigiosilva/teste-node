e326c5d87301fe8e0d0a3fd22e34e39c1ed5a480
Giovani Silva <giovani.m.silva@gmail.com>
2017-11-09 17:20:45 -0200

    Continuando

--- produtosWeb.js
+++ produtosWeb.js
@@ -1,9 +1,16 @@
 var http = require('http');
+var porta = 3000;
+var ip = "localhost";
 
-var server = http.createServer(function(req, res){
-    res.end("<html><body>Listando os produtos</body></html>")
+var server = http.createServer(function(request, response) {
+
+    if(request.url == "/produtos"){
+        response.end('<html><body>Listando produtos da loja</body></html>');
+    }else {
+        response.end('<html><body>Home da casa dos código</body></html>');
+    }
 });
 
-server.listen('3000');
+server.listen(porta, ip);
 
-console.log("Servidor rodando");
+console.log("Server running at http://" + ip + ":" + porta + "/");
